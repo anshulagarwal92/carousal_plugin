f2090118b03c53414976b3a9b459d2c9da6cbfed
Anshul Agarwal <anshul22.03.92@gmail.com>
2015-05-20 17:26:30 +0530

    added an function for infinte carousel and have added an option to move it linearly and circularly(paging is disabled in circular and it is an issue)

--- html/js/carousel.js
+++ html/js/carousel.js
@@ -10,46 +10,67 @@ var carousal = function(kwargs) {
     if(typeof kwargs.container !== "undefined") {
         this.container = kwargs.container;
     }
+
     //it will check if the nextButton is defined or not,if it is not defined then will show an error 
     if(typeof kwargs.nextButton !== "undefined") {
         this.nextButton = kwargs.nextButton;
     }
+
     //it will check if the previousButton is defined or not,if it is not defined then will show an error 
     if(typeof kwargs.previousButton !== "undefined") {
         this.previousButton = kwargs.previousButton;
     }
+
     //it will check if the numberofpages is defined or not,if it is not defined then will show an error 
     if(typeof kwargs.numberofpages !== "undefined") {
         this.numberofpages = kwargs.numberofpages;
     }
+
     //the width of each image is initialized as zero here
     this.eachLiWidth = 0;
+
     this.totalPagesMod = 0;
+
     this.totalLis = 0;
+
     this.myTimer = false;
+
     //the value of the current page is set to 1 
     this.currentPage = 1;
+
     //the value of the total pages is declared and defined as zero 
     this.totalPages = 0;
-
+    this.currentPage1 =1;
     //it will check if is imagesPerPage is defined 
     if(typeof kwargs.imagesPerPage !== "undefined") {
         this.noOfImagesToShowInOnePage = kwargs.imagesPerPage;
     }
-    this.numberOfImageMove = this.noOfImagesToShowInOnePage; 
+
+    this.numberOfImageMove = this.noOfImagesToShowInOnePage;
+
+    if(typeof kwargs.isLinear !== "undefined" && kwargs.isLinear){
+        this.isLinear = kwargs.isLinear;
+    }
+
     //Init plugin
     this.init();
+
     //here we will check that the paging enabled or disabled
     if(typeof kwargs.isPagingEnabled !== "undefined" && kwargs.isPagingEnabled) {
-        this.showPaging();
-        this.highlightPaging();
+            this.showPaging();
+            this.highlightPaging();
     }
+
     //Bind Events.
     this.bindEvents();
 
     if(typeof kwargs.isAutoRotateEnabled !== "undefined" && kwargs.isAutoRotateEnabled) {
-        this.autoRotate();
-    }
+        if(typeof this.isLinear !== "undefined" && this.isLinear ){
+            this.autoRotateLinear();  
+        } else{
+            this.autorotateCircular();
+        }
+    }    
 };
 
 /**
@@ -61,10 +82,14 @@ carousal.prototype = {
     init: function() {
         //this will calculate the width of each image
         this.eachLiWidth = $(this.container+' li:first').width();
+        this.left_indent = (this.eachLiWidth);
+        console.log("this.left_indent",this.left_indent);
         $(this.container).parent().width(this.eachLiWidth*this.noOfImagesToShowInOnePage);
         $(this.container).width(this.eachLiWidth*$(this.container+' li').length);
         //here we are calculating the total number of pages in carousel according to the user input 
         this.totalPages = parseInt((($(this.container+' li').length)) / this.noOfImagesToShowInOnePage);
+
+        this.totalPagesCircular = parseInt(($(this.container+' li').length));
         //we are calculating the Modulus for the remaining number of Images
         this.totalPagesMod = ((($(this.container+' li').length)) % this.noOfImagesToShowInOnePage);
         //we are checking if Modulus is remaining then to add extra one page in pagination
@@ -75,13 +100,20 @@ carousal.prototype = {
 
     //this function is used to show paging or not
     showPaging: function() {
-        for(var i=1;i<=(this.totalPages);i++) {
-            //this is placing the number of pages in the anchor tag inside the div element in html
-            $(this.numberofpages).append('<a href="javascript:void(0)">'+i+'</a>');
+        if(typeof this.isLinear !== "undefined" && this.isLinear ){
+            for(var i=1;i<=(this.totalPages);i++) {
+                //this is placing the number of pages in the anchor tag inside the div element in html
+                $(this.numberofpages).append('<a href="javascript:void(0)">'+i+'</a>');
+            } 
+        } else{
+            for(var i=1;i<=(this.totalPagesCircular);i++) {
+                //this is placing the number of pages in the anchor tag inside the div element in html
+                $(this.numberofpages).append('<a href="javascript:void(0)">'+i+'</a>');
+            }
         }
     },
 
-    autoRotate: function(){
+    autoRotateLinear: function(){
         //Autorotate code for carousel
         var self = this;
         //we will initialize current page with 2 because the page number one is already shown in carousel
@@ -90,58 +122,36 @@ carousal.prototype = {
         self.currentPage = 2;
         //setinterval function is used for setting a particular time interval for a particular function
         self.myTimer = setInterval(function(){
-            //if current page is last then it will move to the first page again
+        //if current page is last then it will move to the first page again
             if(self.currentPage > self.totalPages) {
                 self.currentPage = 1;
             }
-            self.moveToPage(self.currentPage, function() {
+            self.moveToPagelinear(self.currentPage, function() {
                 self.currentPage++;
             });
-        }, 3000);
+        }, 3000); 
     },
 
-    //this function is used to show and hide arrows according to pagination
-    scrollHideAndShowPagination: function() {
+    autorotateCircular:function(){
+        //Autorotate code for carousel
         var self = this;
-        if(self.currentPage >= self.totalPages){
-            $(self.nextButton+" img").hide();
-            $(self.previousButton+" img").show();
-        } else if(self.currentPage <= 1){
-            $(self.previousButton+" img").hide();
-            $(self.nextButton+" img").show();
-        } else if(self.currentPage > 1){
-            $(self.previousButton+" img").show();
-            $(self.nextButton+" img").show();
-        }
-    },
-
-    //this function is used to show and hide arrows according to the click of next and previous buttons
-    scrollHideAndShowButtons:function(){
-        var self = this;
-        if(self.currentPage == 1) {
-            $(self.nextButton+" img").show();
-            $(self.previousButton+" img").hide();
-        }
-        if(self.currentPage > 1 && self.currentPage < self.totalPages){
-            $(self.nextButton+" img").show();
-            $(self.previousButton+" img").show();
-        }
-        if(self.currentPage == self.totalPages) {
-            $(self.nextButton+" img").hide();
-            $(self.previousButton+" img").show();
-        }
-    },
-
-    //this function is used to hide the previous button when the html page is loaded
-    hidePreviousButtonUpFront:function(){
-        var self = this;
-        if(self.currentPage == 1){
-            $(self.previousButton+" img").hide();
-        }
+        //we will initialize current page with 2 because the page number one is already shown in carousel
+        //and if we will put page number 1 instead of 2 then for first animation it will take the double time
+        //as provided in setInterval function and here it will take 6seconds.
+        self.currentPage = 2;
+        self.myTimer = setInterval(function(){
+                //if current page is last then it will move to the first page again
+                if(self.currentPage > self.totalPagesCircular) {
+                    self.currentPage = 1;
+                }
+                self.moveToPageCircular(self.currentPage, function() { 
+                    self.currentPage++;
+                });
+            }, 3000);
     },
 
     //this function is used to animate the carousel when we click on the page number in pagination
-    pageClick :function(){
+    pageClickLinear :function(){
         var self = this;
         $(this.numberofpages+" a").click(function() {
             if(self.myTimer) {
@@ -149,7 +159,20 @@ carousal.prototype = {
             }
             //in currentPage the value is taken from html page that on which page numbr user has clicked
             self.currentPage = parseInt($(this).html());
-            self.moveToPage();
+            self.moveToPagelinear();
+        }); 
+    },
+
+    pageClickCircular :function(){
+        var self = this;
+        $(this.numberofpages+" a").click(function() {
+            if(self.myTimer) {
+                clearInterval(self.myTimer);
+            }
+            //in currentPage the value is taken from html page that on which page numbr user has clicked
+            
+            self.currentPage = parseInt($(this).html());
+            self.moveToPageCircular();
         }); 
     },
 
@@ -166,7 +189,7 @@ carousal.prototype = {
 
     //this is the function which is used for animation according to the clicke page number 
     //and it is also been used in autorotate for the animation when the page is loaded 
-    moveToPage: function(pageNo, callback) {
+    moveToPagelinear: function(pageNo, callback) {
         var self = this;
 
         if(typeof pageNo !== "undefined") {
@@ -214,8 +237,59 @@ carousal.prototype = {
         }
     },
 
+    moveToPageCircular: function(pageNo, callback) {
+        var self = this;
+        if(typeof pageNo !== "undefined") {
+            this.currentPage = pageNo;
+        }
+        $(self.container).animate({
+            'left':"-"+((self.eachLiWidth*self.currentPage)-self.left_indent)
+        },500,function(){
+            //for(i=1;i <= self.currentPage-1 ;i++)
+            //$(self.container+' li:last').after($(self.container+' li:first'));
+            //$(self.container).css({"left":"0px"}); 
+            if(self.currentPage > self.totalPagesCircular){
+                self.currentPage =1;
+            }
+            self.highlightPaging();
+            if(typeof callback !== "undefined")
+                callback();
+        }); 
+    },
+
+    nextButtonClickCircular :function(){
+        var self = this;
+        self.currentPage++;
+        $(self.container).animate({
+            'left':"-="+self.eachLiWidth
+        },500,function(){
+            $(self.container+' li:last').after($(self.container+' li:first'));
+            $(self.container).css({"left":-(self.left_indent)});
+            if(self.currentPage > self.totalPagesCircular){
+                self.currentPage = 1;
+            }
+            self.highlightPaging();
+        }); 
+    },
+
+    //this is used for the anim,ation when user clicks on previous button
+    previousButtonClickCircular :function(){
+        var self = this;
+        self.currentPage--;
+        $(self.container).animate({
+            'left':"+="+self.eachLiWidth
+        },500,function() {
+            $(self.container+' li:first').before($(self.container+' li:last'));
+            $(self.container).css({'left' :-(self.left_indent)});
+            if(self.currentPage < 1){
+                self.currentPage = self.totalPagesCircular;
+            }
+            self.highlightPaging();
+        });
+    },
+
     //this is used for the animation when user clicks on next button
-    nextButtonClick :function(){
+    nextButtonClickLinear :function(){
         var self = this;
         self.currentPage++;
         //if Modulus is zero i.e if we divide number of images to move by the number of images to show in 
@@ -231,7 +305,7 @@ carousal.prototype = {
             }
             if(self.currentPage > self.totalPages){
                 self.currentPage = 1;
-                self.moveToPage(self.currentPage, function() {
+                self.moveToPagelinear(self.currentPage, function() {
                 });
             }
         } else{
@@ -246,7 +320,7 @@ carousal.prototype = {
                 });
                 if(self.currentPage > self.totalPages){
                     self.currentPage = 1;
-                    self.moveToPage(self.currentPage, function() {
+                    self.moveToPagelinear(self.currentPage, function() {
                     });
                 } 
             }else{
@@ -260,7 +334,7 @@ carousal.prototype = {
                 }
                 if(self.currentPage > self.totalPages){
                     self.currentPage = 1;
-                    self.moveToPage(self.currentPage, function() {
+                    self.moveToPagelinear(self.currentPage, function() {
                     });
                 }     
             }
@@ -268,7 +342,7 @@ carousal.prototype = {
     },
 
     //this is used for the anim,ation when user clicks on previous button
-    previousButtonClick :function(){
+    previousButtonClickLinear :function(){
         var self = this;
         //if Modulus is zero i.e if we divide number of images to move by the number of images to show in 
         //one page then if remainder is zero then this loop will execute otherwise else part will execute
@@ -284,7 +358,7 @@ carousal.prototype = {
             }
             if(self.currentPage < 1) {
                 self.currentPage = self.totalPages;
-                self.moveToPage(self.currentPage, function() {
+                self.moveToPagelinear(self.currentPage, function() {
                 });
             }
         } else {
@@ -302,7 +376,7 @@ carousal.prototype = {
                 }
                 if( self.currentPage < 1){
                     self.currentPage = self.totalPages;
-                    self.moveToPage(self.currentPage, function() {
+                    self.moveToPagelinear(self.currentPage, function() {
                     });
                 }
                 
@@ -323,29 +397,56 @@ carousal.prototype = {
     //bindEvents Function
     bindEvents: function() {
         var self = this;
-
+        if(typeof this.linear === "undefined" && !this.isLinear){
+            $(self.container+' li:first').before($(self.container+' li:last'));
+            $(self.container).css({'left' :-(self.left_indent)});
+        }
+        
         //paging click event
-        self.pageClick();
-
-        //Next Button Click event
-        $(self.nextButton+" img").click(function() {
+        if(typeof this.isLinear !== "undefined" && this.isLinear ){
+            self.pageClickLinear();
+        } else{
+            self.pageClickCircular();
+        }
+        if(typeof this.isLinear === "undefined" && !this.isLinear ){
+            $(self.nextButton+" img").click(function() {
             //when user clicks on button the autorotate function will not be working by clearing interval 
             //using clearInterval function
-            if(self.myTimer) {
-                clearInterval(self.myTimer);
-            }
-            self.nextButtonClick();
-        });
-
-        //Previous Button Click Event
-        $(self.previousButton+" img").click(function(){
+                if(self.myTimer) {
+                    clearInterval(self.myTimer);
+                }
+                self.nextButtonClickCircular(); 
+            });
+        } else{
+            $(self.nextButton+" img").click(function() {
             //when user clicks on button the autorotate function will not be working by clearing interval 
             //using clearInterval function
-            if(self.myTimer) {
-                clearInterval(self.myTimer);
-            }
-            self.previousButtonClick();
-        });
+                if(self.myTimer) {
+                    clearInterval(self.myTimer);
+                }
+                self.nextButtonClickLinear(); 
+            });
+        }
+
+        if(typeof this.isLinear === "undefined" && !this.isLinear ){
+            $(self.previousButton+" img").click(function() {
+            //when user clicks on button the autorotate function will not be working by clearing interval 
+            //using clearInterval function
+                if(self.myTimer) {
+                    clearInterval(self.myTimer);
+                }
+                self.previousButtonClickCircular(); 
+            });
+        } else{
+            $(self.previousButton+" img").click(function() {
+            //when user clicks on button the autorotate function will not be working by clearing interval 
+            //using clearInterval function
+                if(self.myTimer) {
+                    clearInterval(self.myTimer);
+                }
+                self.previousButtonClicka(); 
+            });
+        }
     }
 };
 
@@ -357,7 +458,8 @@ $(document).ready(function() {
         "previousButton": "#left_scroll",
         "numberofpages":"#numofpages",
         "isPagingEnabled": true,
-        "imagesPerPage":3,
-        "isAutoRotateEnabled": true
+        "imagesPerPage":4,
+        "isAutoRotateEnabled": false,
+        "isLinear":false
     });
 });
